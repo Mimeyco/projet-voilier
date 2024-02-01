92a408417b573e30b6150bfa10fe7a07354d5c14
Loghan <loghan.meynaud@gmail.com>
2024-02-01 23:13:58 +0100

    Ajout des sections newspaper, slider et contact. Section contact en cours de stylisation

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // DEFILEMENT IMAGES LANDING
 const landing = document.querySelector(".landing-container");
-console.log(landing.classList);
 
 document.addEventListener("DOMContentLoaded", function () {
   let i = 1;
@@ -30,3 +29,93 @@ function updateCountdown() {
 updateCountdown();
 // Mise à jour toutes les 24 heures (86400000 millisecondes)
 setInterval(updateCountdown, 86400000);
+
+//SLIDER
+
+const rightChevron = document.getElementById("chevron-right");
+const leftChevron = document.getElementById("chevron-left");
+const slides = document.querySelectorAll(".animation");
+
+rightChevron.addEventListener("click", slideToRight);
+leftChevron.addEventListener("click", slideToLeft);
+
+const lastSlideIndex = slides.length - 1;
+
+let lock = false;
+let indexSlideOnScreen = 0;
+function slideToRight() {
+  if (!lock) {
+    lock = true;
+    slides[indexSlideOnScreen].classList.add("disparition-right");
+    slides[indexSlideOnScreen].classList.remove(
+      ...["apparition-left", "apparition-right"]
+    );
+    cleanClasses(indexSlideOnScreen);
+    setTimeout(() => {
+      if (indexSlideOnScreen + 1 > 2) {
+        slides[0].classList.remove("hidden");
+        slides[0].classList.add("apparition-right");
+      } else {
+        slides[indexSlideOnScreen + 1].classList.remove("hidden");
+        slides[indexSlideOnScreen + 1].classList.add("apparition-right");
+      }
+      if (indexSlideOnScreen === lastSlideIndex) {
+        indexSlideOnScreen = 0;
+      } else {
+        indexSlideOnScreen++;
+      }
+      lock = false;
+    }, 800);
+  }
+}
+
+function slideToLeft() {
+  if (!lock) {
+    lock = true;
+    slides[indexSlideOnScreen].classList.add("disparition-left");
+    slides[indexSlideOnScreen].classList.remove(
+      ...["apparition-left", "apparition-right"]
+    );
+    cleanClasses(indexSlideOnScreen);
+    setTimeout(() => {
+      if (indexSlideOnScreen - 1 < 0) {
+        slides[lastSlideIndex].classList.remove("hidden");
+        slides[lastSlideIndex].classList.add("apparition-left");
+      } else {
+        slides[indexSlideOnScreen - 1].classList.remove("hidden");
+        slides[indexSlideOnScreen - 1].classList.add("apparition-left");
+      }
+      if (indexSlideOnScreen === 0) {
+        indexSlideOnScreen = lastSlideIndex;
+      } else {
+        indexSlideOnScreen--;
+      }
+      lock = false;
+    }, 800);
+  }
+}
+
+function cleanClasses(index) {
+  let classesToRemove = [
+    "disparition-right",
+    "disparition-left",
+    "apparition-right",
+    "apparition-left",
+  ];
+  if (index === 0) {
+    slides[index + 1].classList.remove(...classesToRemove);
+    slides[index + 1].classList.add("hidden");
+    slides[lastSlideIndex].classList.remove(...classesToRemove);
+    slides[lastSlideIndex].classList.add("hidden");
+  } else if (index === lastSlideIndex) {
+    slides[index - 1].classList.remove(...classesToRemove);
+    slides[index - 1].classList.add("hidden");
+    slides[0].classList.remove(...classesToRemove);
+    slides[0].classList.add("hidden");
+  } else {
+    slides[index + 1].classList.remove(...classesToRemove);
+    slides[index + 1].classList.add("hidden");
+    slides[index - 1].classList.remove(...classesToRemove);
+    slides[index - 1].classList.add("hidden");
+  }
+}

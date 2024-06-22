9599471d6b6e1aaaaeee2f12403adae173bf9555
Loghan <loghan.meynaud@gmail.com>
2024-06-22 22:24:25 +0200

    Ajout d'un modal pour les caractéristiques du bateau + suppression de la page technique

--- main.js
+++ main.js
@@ -40,6 +40,33 @@ updateCountdown();
 // Mise à jour toutes les 24 heures (86400000 millisecondes)
 setInterval(updateCountdown, 86400000);
 
+//MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalTrigger");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 //SLIDER
 const slides = [...document.querySelectorAll(".slide")];
 

9f137738fb8eb38c8f1ae7ec09256918e32f61c7
Loghan <loghan.meynaud@gmail.com>
2024-09-22 22:18:59 +0200

    Modification compte à rebour navbar par départ depuis le 21/09/2024

--- js/common.js
+++ js/common.js
@@ -35,20 +35,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function updateCountdown() {
   // Date cible de départ (10 septembre 2024)
-  let targetDate = new Date("2024-09-21T00:00:00Z");
-  let currentDate = new Date();
-  let timeDifference = targetDate.getTime() - currentDate.getTime();
-  let daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  // let targetDate = new Date("2024-09-21T00:00:00Z");
+  // let currentDate = new Date();
+  // let timeDifference = targetDate.getTime() - currentDate.getTime();
+  // let daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
 
   // Affichage dans le span avec l'id "countdown"
   let countdownSpan = document.querySelectorAll("#counting-days");
   countdownSpan.forEach((e) => {
     if (e) {
-      e.textContent = daysRemaining;
+      e.textContent = "21/09/2024";
     }
   });
 }
 
 updateCountdown();
 // Mise à jour toutes les 24 heures (86400000 millisecondes)
-setInterval(updateCountdown, 86400000);
+// setInterval(updateCountdown, 86400000);
